5b5b53b8bf1e16432020c47dcab7f02827b2308a
jye <jordilele@gmail.com>
2023-10-20 09:23:43 +0200

    creando clase Allow

---
+++ src/baseNoStates/AllowAccess.java
@@ -0,0 +1,13 @@
+package baseNoStates;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AllowAccess {
+  private User user;
+  private LocalDate startPeriod;
+  private LocalDate endPeriod;
+  private LocalTime startHour;
+  private LocalTime endHour;
+
+}
