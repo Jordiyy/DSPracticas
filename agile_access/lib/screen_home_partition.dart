d198615a68d8b8986ab5d89d4773c4889e7f3e93
Christina de Leon <christianjls@outlook.com>
2023-12-12 22:01:31 +0100

    revisar areas y puertas

---
+++ agile_access/lib/screen_home_partition.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/mdi.dart';
+
+import 'package:agile_access/data/user_data.dart';
+import 'screen_space.dart';
+
+import 'data/door_tree.dart';
+
+class ScreenHomePartition extends StatefulWidget {
+  UserGroup userGroup;
+  User userData;
+  String areaName;
+
+  ScreenHomePartition(
+      {super.key,
+      required this.userGroup,
+      required this.userData,
+      required this.areaName});
+
+  @override
+  State<ScreenHomePartition> createState() => _ScreenHomePartition();
+}
+
+class _ScreenHomePartition extends State<ScreenHomePartition> {
+  late UserGroup userGroup;
+  late User userData;
+  late String areaName;
+  late Tree doorTree;
+
+  List<bool> switchValue = [false, false, false];
+  String iconImgBuilding = Mdi.office_building_remove;
+
+  @override
+  void initState() {
+    super.initState();
+    userGroup = widget.userGroup;
+    userData = widget.userData;
+    areaName = widget.areaName;
+    doorTree = getTree(areaName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          title: Text(areaName == "building" ? "Home" : areaName),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(areaName == "building"
+                    ? "Building floors"
+                    : "${areaName} areas"),
+                Visibility(
+                    visible: areaName == "building",
+                    child: IconButton(
+                      icon: Iconify(iconImgBuilding),
+                      onPressed: () {
+                        setState(() {
+                          bool allFalse =
+                              switchValue.every((value) => value == true);
+                          switchValue =
+                              List.filled(switchValue.length, !allFalse);
+                          iconImgBuilding = allFalse
+                              ? Mdi.office_building_remove
+                              : Mdi.office_building;
+                        });
+                      },
+                    ))
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(16.0),
+                itemCount: doorTree.root.children.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    _buildRow(doorTree.root.children[index], index),
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget _buildRow(Area area, int index) {
+    String iconIMG;
+
+    if (index == 0) {
+      iconIMG = Mdi.home_floor_negative_1;
+    } else if (index == 1) {
+      iconIMG = Mdi.home_floor_0;
+    } else {
+      iconIMG = Mdi.home_floor_1;
+    }
+    return GestureDetector(
+      onTap: () {
+        if (doorTree.root.children[index] is Partition) {
+          Navigator.of(context).push(MaterialPageRoute<void>(
+            builder: (context) => ScreenHomePartition(
+                userGroup: userGroup,
+                userData: userData,
+                areaName: doorTree.root.children[index].id),
+          ));
+        }
+        if (doorTree.root.children[index] is Space) {
+          Navigator.of(context).push(MaterialPageRoute<void>(
+            builder: (context) => ScreenSpace(
+                userGroup: userGroup,
+                userData: userData,
+                areaName: doorTree.root.children[index].id),
+          ));
+        }
+      },
+      child: Card(
+          child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: [
+                  Iconify(iconIMG),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        area.id,
+                        style: const TextStyle(fontSize: 15.0),
+                      ),
+                      Text('${doorTree.root.children.length}' " Areas",
+                          style: const TextStyle(fontSize: 15.0)),
+                      const Text(
+                        "0 Locked doors, 0 Unlocked doors",
+                        style: TextStyle(fontSize: 15.0),
+                      )
+                    ],
+                  ),
+                  Switch(
+                      value: switchValue[index],
+                      onChanged: (bool value) {
+                        setState(() {
+                          switchValue[index] = value;
+                        });
+                      }),
+                ],
+              ))),
+    );
+  }
+}
---
+++ agile_access/lib/screen_space.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/bi.dart';
+
+import 'package:agile_access/data/user_data.dart';
+import 'data/door_tree.dart';
+
+class ScreenSpace extends StatefulWidget {
+  UserGroup userGroup;
+  User userData;
+  String areaName;
+
+  ScreenSpace(
+      {super.key,
+      required this.userGroup,
+      required this.userData,
+      required this.areaName});
+
+  @override
+  State<ScreenSpace> createState() => _ScreenSpace();
+}
+
+class _ScreenSpace extends State<ScreenSpace> {
+  late UserGroup userGroup;
+  late User userData;
+  late String areaName;
+  late Tree doorTree;
+
+  List<bool> switchValue = [false, false, false];
+
+  @override
+  void initState() {
+    super.initState();
+    userGroup = widget.userGroup;
+    userData = widget.userData;
+    areaName = widget.areaName;
+    doorTree = getTree(areaName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          title: Text(areaName == "building" ? "Home" : areaName),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("${areaName} doors"),
+            Text("You have acces to "
+                '${doorTree.root.children.length}'
+                " doors"),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(16.0),
+                itemCount: doorTree.root.children.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    _buildRow(doorTree.root.children[index], index),
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget _buildRow(Door door, int index) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+          child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: [
+                  Iconify(Bi.door_closed),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        door.id,
+                        style: const TextStyle(fontSize: 15.0),
+                      ),
+                      Text("Unlocked - Closed",
+                          style: const TextStyle(fontSize: 15.0)),
+                    ],
+                  ),
+                ],
+              ))),
+    );
+  }
+}
